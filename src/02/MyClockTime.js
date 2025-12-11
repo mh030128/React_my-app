d62f505f3c2cc8e7fd2b25328bb4079102eb8ddb
sujin kim <mh030128@naver.com>
2025-12-11 15:19:47 +0900

    lecture02

---
+++ src/02/MyClockTime.js
@@ -0,0 +1,12 @@
+function MyClockTime () {
+
+    return(
+        <div>
+            현재 시각 : {new Date().toLocaleTimeString()}
+            
+        </div>
+       
+    );
+}
+
+export default MyClockTime ;
