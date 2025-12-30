7b8bd73167f54b018df064268e98d81159f2224a
sujin kim <mh030128@naver.com>
2025-12-30 18:58:01 +0900

    Clock_useEffect_Ex

--- src/02/MyClockTime.js
+++ src/02/MyClockTime.js
@@ -1,11 +1,55 @@
+// import styles from './MyClockTime.module.css';
+// import { useState, useEffect } from 'react';
+
+// function MyClockTime () {
+
+//     const [now, setCtime] = useState(new Date());
+
+//     useEffect(() => {
+//         const intervalId = setInterval(() => {
+//             setCtime(new Date());
+//         }, 1000);
+
+//         return () => {
+//             clearInterval(intervalId);
+//         }
+//     }, []);
+
+//     return(
+//         <div className={styles.c3}>
+//             현재 시각 : {now.toLocaleTimeString()}            
+//         </div>    
+//     );
+// }
+
+// export default MyClockTime ;
+
+import styles from './MyClockTime.module.css';
+import { useState, useEffect, useMemo } from 'react';
+
 function MyClockTime () {
 
+    const [now, setNow] = useState(() => new Date());
+
+    const timeFormatter = useMemo(() =>
+        new Intl.DateTimeFormat("ko-KR", {
+            hour: "2-digit",
+            minute: "2-digit",
+            second: "2-digit",
+        }), []);
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setNow(new Date());
+        }, 1000);
+
+        return () => window.clearInterval(intervalId);
+    }, []);
+
     return(
-        <div>
-            현재 시각 : {new Date().toLocaleTimeString()}
-            
-        </div>
-       
+        <div className={styles.c3}>
+            현재 시각 : {timeFormatter.format(now)}            
+        </div>    
     );
 }
 
--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 // import logo from './logo.svg';
 import './App.css';
 // import Hello from './01/Hello';
-// import MyClock from './02/MyClock';
+import MyClock from './02/MyClock';
 //import MyDiv01 from './03/MyDiv01';
 // import MyList from './04/MyList';
 
-import Lotto from './05/Lotto';
+// import Lotto from './05/Lotto';
 import { RiHomeHeartFill } from "react-icons/ri";
 function App() {
 
@@ -18,7 +18,8 @@ function App() {
       <main className='grow w-full flex justify-center items-center overflow-y-auto'>
         {/* <MyDiv01 /> */}
         {/* <MyList /> */}
-        <Lotto />
+        {/* <Lotto /> */}
+        <MyClock />
       </main>
       <footer className='flex justify-center items-center h-20 bg-black text-slate-100'>
         ⓒ Kim Su Jin
